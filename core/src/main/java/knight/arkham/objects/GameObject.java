f0e404c5ea668dddf2ac611cbfdadb076dbd3fef
knight <karvin_jimenez@hotmail.com>
2023-11-20 22:57:15 -0400

    added html support.

--- core/src/main/java/knight/arkham/objects/GameObject.java
+++ core/src/main/java/knight/arkham/objects/GameObject.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
+
 import static knight.arkham.helpers.Constants.PIXELS_PER_METER;
 
 public abstract class GameObject {
--- core/src/main/java/knight/arkham/objects/structures/InteractiveStructure.java
+++ core/src/main/java/knight/arkham/objects/structures/InteractiveStructure.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Filter;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.World;
+
 import static knight.arkham.helpers.Constants.DESTROYED_BIT;
 import static knight.arkham.helpers.Constants.PIXELS_PER_METER;
 
